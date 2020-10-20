d65e3b3f65d1e7955637ca4f51dba0b2a3cd82e7
andrejborcenko <andrei-borchenko@mail.ru>
2020-10-20 10:44:11 +0300

    Реализовать Store<T extends Base> [#374869]

--- src/main/java/Job4j/it/generics/MemStore.java
+++ src/main/java/Job4j/it/generics/MemStore.java
@@ -13,6 +13,7 @@ public class MemStore<T extends Base> implements Store<T> {
         for (int i = 0; i < mem.size(); i++) {
             if(id.equals(mem.get(i).getId())) {
                 index = i;
+                break;
             }
         }
         return index;
