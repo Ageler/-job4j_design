1a80cb3935073b6d2a59554a346a8bcb5f949077
andrejborcenko <andrei-borchenko@mail.ru>
2020-11-06 15:14:32 +0300

    Создать контейнер на базе связанного списка [#374848]

---
+++ src/main/java/Job4j/it/list/Node.java
@@ -0,0 +1,15 @@
+package Job4j.it.list;
+
+import java.util.LinkedList;
+
+public class Node<E> {
+    E item;
+    Node<E> next;
+    Node<E> prev;
+
+    Node(Node<E> prev, E element, Node<E> next) {
+        this.item = element;
+        this.next = next;
+        this.prev = prev;
+    }
+}
