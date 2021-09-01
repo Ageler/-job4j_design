f7c868a1c1b3b27c6e9bebe1a73fcc72338bf32e
Ageler <andrei-borchenko@mail.ru>
2021-09-01 04:53:00 +0300

    Тестирование IO [#173905]

---
+++ src/main/java/Job4j/it/InputOutput/Abuse.java
@@ -0,0 +1,19 @@
+package Job4j.it.InputOutput;
+
+import java.io.*;
+import java.util.List;
+
+import static java.util.stream.Stream.of;
+
+
+public class Abuse {
+    public static void drop(String source, String target, List<String> words) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            in.lines()
+                    .flatMap(line -> of(line.split("\\s+")))
+                    .filter(word -> !words.contains(word)).map(word -> word + " ")
+                    .forEach(out::print);
+        }
+    }
+}
--- src/main/java/Job4j/it/InputOutput/Analizy.java
+++ src/main/java/Job4j/it/InputOutput/Analizy.java
@@ -1,18 +1,16 @@
 package Job4j.it.InputOutput;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class Analizy {
-    public void unavailable(String source, String target) throws FileNotFoundException {
+
+    public static void unavailable(String source, String target) throws FileNotFoundException {
         boolean status = false;
         BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
         List<String> res = bufferedReader.lines().collect(Collectors.toList());
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("src/main/java/Job4j/it/InputOutput/unavailable.csv"))) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             for (String s : res) {
                 if (((s.contains("400 ") || s.contains("500 ")) && !status)) {
                    out.print(s.split(" ")[1] + ";");
